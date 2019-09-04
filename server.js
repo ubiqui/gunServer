9d4d7c733018da3a42308170f8f86ec37c6355a8
ubiqui <msareika@web.de>
2019-09-04 09:55:53 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+﻿var Gun     = require('gun');
+require('gun/axe');
+var fs = require('fs');
+var http = require('http');
+const handler = require('serve-handler');
+
+const server = http.createServer((request, response) => {
+	return handler(request, response, {
+	  "public": "www"
+	});
+});
+
+server.listen(8080, function (err) {
+	if (err) {
+		return;
+	}
+	
+	var gun = Gun({	file: 'data', web: sslServer });
+	global.Gun = Gun; /// make global to `node --inspect` - debug only
+	global.gun = gun; /// make global to `node --inspect` - debug only
+
+	console.log('gun server running');
+}); 
+
